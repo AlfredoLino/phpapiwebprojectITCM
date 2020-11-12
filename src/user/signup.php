d26da345622a5a9ababd98c9d08f7af5dd32fb9e
Lino <alfredolinomendoza@gmail.com>
2020-11-12 14:53:00 -0600

    Validacion temporal de datos

--- src/user/signup.php
+++ src/user/signup.php
@@ -8,13 +8,27 @@ use Firebase\JWT\JWT;
 include_once "../connectionDB.php";
 $dbconnection = new conexionServer();
 $_POST = json_decode(file_get_contents('php://input'), true);
-$dbconnection->signinProfesor($_POST["nombre"], $_POST["apellidos"],$_POST["email"], $_POST["celular"],$_POST["pass"]);
-if($dbconnection){
-    $res = array("response" => $dbconnection);
-    echo json_encode($res);
+
+$invalidReq = false;
+
+foreach($_POST as $key => $value){
+    $actual = $_POST[$key];
+    if(!(isset($actual) && $actual != "")){
+        $invalidReq = true;
+    }
+}
+
+if(!$invalidReq) {
+    $dbconnection->signinProfesor($_POST["nombre"], $_POST["apellidos"], $_POST["email"], $_POST["celular"], $_POST["pass"]);
+    if ($dbconnection) {
+        $res = array("response" => true, "message" => "Usuario creado de manera satisfactoria");
+        echo json_encode($res);
+    } else {
+        $res = array("response" => false, "message" => "Error al ingresar a la base de datos del sistema");
+        echo json_encode($res);
+    }
 }else{
-    $res = array("response" => false);
-    echo json_encode($res);
+    echo json_encode(array("response" => false, "message" => "Datos invalidos. Por favor, corrija."));
 }
 /*$conexion = new conexionServer();
 
